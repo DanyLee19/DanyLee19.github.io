6074504b43da805d636dcd9f427daefc4e882288
DanyLee19 <52714210+DanyLee19@users.noreply.github.com>
2025-08-11 02:18:15 +0000

    Deploying to gh-pages from @ DanyLee19/DanyLee19.github.io@515a55f2a88ad3680120fd31dabf9128458077b3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "",
+          description: "† indicates corresponding author; * indicates co-primary author.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
